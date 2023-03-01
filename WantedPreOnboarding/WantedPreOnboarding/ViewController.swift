fb3c169da9afe7a50166627ea4c2d99609ffb010
kpk0616 <kpk0616@naver.com>
2023-03-01 20:14:57 +0900

    [Add] iOS 프로젝트로 재생성

--- WantedPreOnboarding/WantedPreOnboarding/ViewController.swift
+++ WantedPreOnboarding/WantedPreOnboarding/ViewController.swift
@@ -5,20 +5,13 @@
 //  Created by 박의서 on 2023/03/01.
 //
 
-import Cocoa
+import UIKit
 
-class ViewController: NSViewController {
+class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-
     // Do any additional setup after loading the view.
-  }
-
-  override var representedObject: Any? {
-    didSet {
-    // Update the view, if already loaded.
-    }
   }
 
 
