faf83dfa8005e66f8db165e2da69e95987c0bd7f
kpk0616 <kpk0616@naver.com>
2023-03-01 20:10:35 +0900

    [Add] 프로젝트 생성

---
+++ WantedPreOnboarding/WantedPreOnboarding/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WantedPreOnboarding
+//
+//  Created by 박의서 on 2023/03/01.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
