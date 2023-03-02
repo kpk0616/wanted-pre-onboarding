3b5f79bd05d6821bede93315024a51dde2e0424e
kpk0616 <kpk0616@naver.com>
2023-03-02 17:23:10 +0900

    [Add] UIImageView+Extension 추가

---
+++ WantedPreOnboarding/WantedPreOnboarding/Global/UIImageView+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIImageView+Extension.swift
+//  WantedPreOnboarding
+//
+//  Created by 박의서 on 2023/03/02.
+//
+
+import UIKit
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
